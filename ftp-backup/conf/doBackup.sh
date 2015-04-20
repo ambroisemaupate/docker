ccf7414470c3eee2256dc1fde921c4ba20d371c2
Ambroise Maupate <ambroise.maupate@gmail.com>
2015-04-21 01:46:02 +0200

    Removed tar extension from db dump. Dumb!

--- ftp-backup/conf/doBackup.sh
+++ ftp-backup/conf/doBackup.sh
@@ -9,7 +9,7 @@ FILE_DATE=`date +%Y%m%d-%H%M`
 # Data backup
 $TAR $TAR_OPTIONS /backups/data-$FILE_DATE.tar.gz /data
 # MySQL dump
-$MYSQLDUMP -u $DB_USER -h $DB_HOST -p$DB_PASS $DB_NAME | gzip > /backups/db-$FILE_DATE.tar.gz
+$MYSQLDUMP -u $DB_USER -h $DB_HOST -p$DB_PASS $DB_NAME | gzip > /backups/db-$FILE_DATE.gz
 
 # Sending over FTP
 lftp -u ${FTP_USER},${FTP_PASS} ftp://${FTP_HOST}:${FTP_PORT} <<EOF
@@ -19,7 +19,7 @@ cd ${REMOTE_PATH}
 ls
 
 put /backups/data-${FILE_DATE}.tar.gz
-put /backups/db-${FILE_DATE}.tar.gz
+put /backups/db-${FILE_DATE}.gz
 
 bye
 
