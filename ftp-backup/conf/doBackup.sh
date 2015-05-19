e58772ab57d120e6212b76ed1dccff313de8bd84
Ambroise Maupate <ambroise@rezo-zero.com>
2015-05-19 11:08:37 +0200

    Removed indent

--- ftp-backup/conf/doBackup.sh
+++ ftp-backup/conf/doBackup.sh
@@ -1,6 +1,7 @@
 #/bin/bash
 MYSQLDUMP="$(which mysqldump)"
 TAR="$(which tar)"
+LFTP="$(which lftp)"
 TAR_OPTIONS="-zcf"
 FILE_DATE=`date +%Y%m%d-%H%M`
 
@@ -8,19 +9,19 @@ echo "[${FILE_DATE}] New backup --------"
 
 if [ -n "$FTP_HOST" ]
 then
-    # Create remote dir if does not exists
-    lftp -u ${FTP_USER},${FTP_PASS} ftp://${FTP_HOST}:${FTP_PORT} <<EOF
+# Create remote dir if does not exists
+$LFTP -u ${FTP_USER},${FTP_PASS} ftp://${FTP_HOST}:${FTP_PORT} <<EOF
 
 mkdir -p ${REMOTE_PATH}
 bye
 
 EOF
 
-    # Data backup
-    $TAR $TAR_OPTIONS /backups/data-$FILE_DATE.tar.gz /data
+# Data backup
+$TAR $TAR_OPTIONS /backups/data-$FILE_DATE.tar.gz /data
 
-    # Sending over FTP
-    lftp -u ${FTP_USER},${FTP_PASS} ftp://${FTP_HOST}:${FTP_PORT} <<EOF
+# Sending over FTP
+$LFTP -u ${FTP_USER},${FTP_PASS} ftp://${FTP_HOST}:${FTP_PORT} <<EOF
 
 cd ${REMOTE_PATH}
 ls
@@ -29,15 +30,15 @@ bye
 
 EOF
 
-    # MySQL dump
-    if [ -n "$DB_NAME" ]
-    then
-        echo "MySQL dump backup ---"
-        # MySQL dump
-        $MYSQLDUMP -u $DB_USER -h $DB_HOST -p$DB_PASS $DB_NAME | gzip > /backups/db-$FILE_DATE.sql.gz
+# MySQL dump
+if [ -n "$DB_NAME" ]
+then
+echo "MySQL dump backup ---"
+# MySQL dump
+$MYSQLDUMP -u $DB_USER -h $DB_HOST -p$DB_PASS $DB_NAME | gzip > /backups/db-$FILE_DATE.sql.gz
 
-        # Sending over FTP
-        lftp -u ${FTP_USER},${FTP_PASS} ftp://${FTP_HOST}:${FTP_PORT} <<EOF
+# Sending over FTP
+$LFTP -u ${FTP_USER},${FTP_PASS} ftp://${FTP_HOST}:${FTP_PORT} <<EOF
 
 cd ${REMOTE_PATH}
 ls
@@ -45,5 +46,5 @@ put /backups/db-${FILE_DATE}.sql.gz
 bye
 
 EOF
-    fi
+fi
 fi
