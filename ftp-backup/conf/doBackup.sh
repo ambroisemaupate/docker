dfcc14d8cb5703cb996d7166a91223816e2bd199
Ambroise Maupate <ambroise@rezo-zero.com>
2018-04-21 21:50:39 +0200

    Better ftp script

--- ftp-backup/conf/doBackup.sh
+++ ftp-backup/conf/doBackup.sh
@@ -1,20 +1,21 @@
-#/bin/bash
+#!/usr/bin/env bash
+# Author: Ambroise Maupate
+
 MYSQLDUMP="$(which mysqldump)"
 TAR="$(which tar)"
 LFTP="$(which lftp)"
+GZIP="$(which gzip)"
 TAR_OPTIONS="-zcf"
 FILE_DATE=`date +%Y%m%d-%H%M`
-PRINT_DATE=`date '+%Y-%m-%d %H:%M:%S'`
 
-echo "[${PRINT_DATE}] --> New backup"
-echo "[${PRINT_DATE}] On ${FTP_PROTO}://${FTP_HOST}:${FTP_PORT}"
+echo "[`date '+%Y-%m-%d %H:%M:%S'`] Begining new backup on ${FTP_PROTO}://${FTP_HOST}:${FTP_PORT}"
 
 LFTP_CMD="-u ${FTP_USER},${FTP_PASS} ${FTP_PROTO}://${FTP_HOST}:${FTP_PORT}"
 
 if [ -n "$FTP_HOST" ]
 then
 # Create remote dir if does not exists
-echo "[${PRINT_DATE}] Create remote dir if does not exists…"
+echo "[`date '+%Y-%m-%d %H:%M:%S'`] Create remote dir if does not exists…"
 ${LFTP} ${LFTP_CMD} <<EOF
 cd ${REMOTE_PATH} || mkdir -p ${REMOTE_PATH};
 bye;
@@ -22,42 +23,41 @@ EOF
 
 if [ -d ${LOCAL_PATH} ]; then
   # Control will enter here if /data exists.
-  echo "[${PRINT_DATE}] Compressing ${LOCAL_PATH} folder…"
+  echo "[`date '+%Y-%m-%d %H:%M:%S'`] Compressing ${LOCAL_PATH} folder…"
   $TAR $TAR_OPTIONS /backups/data-$FILE_DATE.tar.gz ${LOCAL_PATH}
 
   # Sending over FTP
-  echo "[${PRINT_DATE}] Sending ${LOCAL_PATH} folder over FTP…"
+  echo "[`date '+%Y-%m-%d %H:%M:%S'`] Sending ${LOCAL_PATH} folder over FTP…"
   ${LFTP} ${LFTP_CMD} <<EOF
-cd ${REMOTE_PATH}
-ls
-put /backups/data-${FILE_DATE}.tar.gz
-bye
+cd ${REMOTE_PATH};
+ls;
+put /backups/data-${FILE_DATE}.tar.gz;
+bye;
 EOF
 else
-  echo "[${PRINT_DATE}] ${LOCAL_PATH} folder does not exists."
+  echo "[`date '+%Y-%m-%d %H:%M:%S'`] ${LOCAL_PATH} folder does not exists."
   exit 1
 fi
 
 
 # Optional MySQL dump
 if [ -n "$DB_NAME" ]
-  echo "[${PRINT_DATE}] No database to backup."
+  echo "[`date '+%Y-%m-%d %H:%M:%S'`] No database to backup."
 then
 # MySQL dump
-echo "[${PRINT_DATE}] MySQL dump backup…"
-$MYSQLDUMP -u $DB_USER -h $DB_HOST -p$DB_PASS $DB_NAME | gzip > /backups/db-$FILE_DATE.sql.gz
+echo "[`date '+%Y-%m-%d %H:%M:%S'`] MySQL dump backup…"
+$MYSQLDUMP -u $DB_USER -h $DB_HOST --password=$DB_PASS $DB_NAME | gzip > /backups/db-$FILE_DATE.sql.gz
 
 # Sending over FTP
-echo "[${PRINT_DATE}] Sending MySQL dump over FTP…"
+echo "[`date '+%Y-%m-%d %H:%M:%S'`] Sending MySQL dump over FTP…"
 ${LFTP} ${LFTP_CMD} <<EOF
-cd ${REMOTE_PATH}
-ls
-put /backups/db-${FILE_DATE}.sql.gz
-bye
+cd ${REMOTE_PATH};
+ls;
+put /backups/db-${FILE_DATE}.sql.gz;
+bye;
 EOF
 fi
 fi
 
-PRINT_ENDDATE=`date '+%H:%M:%S'`
-echo "[${PRINT_DATE}] Backup finished at ${PRINT_ENDDATE}."
+echo "[`date '+%Y-%m-%d %H:%M:%S'`] Backup finished"
 
