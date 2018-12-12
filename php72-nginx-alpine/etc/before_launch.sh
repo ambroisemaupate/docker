9e40b84d81508d9a5881446f1f999b25c0ab4d20
Ambroise Maupate <ambroise@rezo-zero.com>
2018-12-12 23:50:38 +0100

    Added new php56-fpm-alpine for legacy projects compatibility

--- php72-nginx-alpine/etc/before_launch.sh
+++ php72-nginx-alpine/etc/before_launch.sh
@@ -1,2 +1,4 @@
 # Fix volume permissions
-exec chown -R www-data:www-data /var/www/html/
+exec chown -R www-data:www-data /var/www/html/files
+exec chown -R www-data:www-data /var/www/html/web
+exec chown -R www-data:www-data /var/www/html/app
