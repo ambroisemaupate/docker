b34143ca5027c829d2df99ea52ee0fb56d34d0d8
Ambroise Maupate <ambroise@rezo-zero.com>
2021-03-18 18:21:39 +0100

    Fixed php72-nginx tmp dirs

--- light-ssh/start.sh
+++ light-ssh/start.sh
@@ -6,15 +6,11 @@ if ( id ${USER} ); then
     echo "INFO: User ${USER} already exists"
     echo "INFO: Change ${USER} password with environment:"
     echo "${USER}:${PASS}"|chpasswd
-
-    if [ "$USER" = "root" ]; then
-        echo "INFO: Permit ${USER} login:"
-        sed -i 's/PermitRootLogin no/PermitRootLogin yes/g' /etc/ssh/sshd_config
-    fi
 else
     echo "INFO: User ${USER} does not exist, we create it"
     ENC_PASS=$(perl -e 'print crypt($ARGV[0], "password")' ${PASS})
-    useradd -d /data -m -p ${ENC_PASS} -u ${USER_UID} -s /bin/bash ${USER}
+    groupadd --gid ${USER_UID} ${USER}
+    useradd -d /data -m -p ${ENC_PASS} --uid ${USER_UID} --gid ${USER_UID} -s /bin/bash ${USER}
 fi
 
 if [ ! -f /etc/ssh/ssh_host_rsa_key ]; then
